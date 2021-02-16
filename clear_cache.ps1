44562c52a6459a24e15bf5dea858682a6871c009
Gallium Wang <galliumwang199@gmail.com>
2021-02-16 22:47:33 +0800

    add comment for clear_cache.ps1

--- clear_cache.ps1
+++ clear_cache.ps1
@@ -1,5 +1,5 @@
 # rm all file except clear_cache.ps1 in specific folder
-
+# if exclude arg not specifed.the script itself will be deleted
 $script_name="clear_cache.ps1"
 $location="C:\Users\26340\Desktop\tmp\*"
 
