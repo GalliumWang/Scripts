6870b43b8aa85cff33855f7f8578753d9bdc82b1
Gallium Wang <galliumwang199@gmail.com>
2021-02-27 23:57:26 +0800

    minor update

--- clear_cache.ps1
+++ clear_cache.ps1
@@ -1,6 +1,6 @@
 # rm all file except clear_cache.ps1 in specific folder
 # if exclude arg not specifed.the script itself will be deleted
-$script_name="clear_cache.ps1"
+$script_name="clear_cache.ps1"  # name of this script self
 $location="path/to/folder/*"
 
 Remove-Item -Recurse -Force -Exclude $script_name -Path $location
